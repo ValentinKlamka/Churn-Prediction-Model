c1e33e9c0f5e54694f91325e812baada6e166e62
Valentin Klamka <valentin.klamka@googlemail.com>
2022-08-25 16:24:32 +0200

    Übersetzung ins Deutsche

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,54 +49,141 @@ def generate_predictions(test_df):
 
 
 if __name__ == "__main__":
-    st.title("Telco Churn Prediction")
-    st.subheader("Enter data about the cusomter you would like to predict:")
+    st.title("Telcos Vorhersage zur Abwanderung")
+    st.subheader("Geben Sie die Daten ihres Kunden ein, dessen Verhalten Sie vorhersagen wollen:")
 
     # making customer data inputs
-    gender = st.selectbox("Select customer's gender:",
-                          ['Female', "Male"])
-    senior_citizen_input = st.selectbox("Is customer a senior citizen?:",
-                                        ["Yes", "No"])
-    if senior_citizen_input == "Yes":
+    gender = st.selectbox("Welches Geschlecht hat der Kunde:",
+                          ['Weiblich', "Männlich"])
+    if gender == "Weiblich":
+        gender = "Female"
+    else:
+        gender = "Male"
+    senior_citizen_input = st.selectbox("Ist der Kunde in Rente?:",
+                                        ["Ja", "Nein"])
+    if senior_citizen_input == "Ja":
         senior_citizen = 1
     else:
         senior_citizen = 0
 
-    partner = st.selectbox("Does the customer have a partner?:",
-                          ["Yes", "No"])
-    dependents = st.selectbox("Does the customer have dependents?:",
-                             ["Yes", "No"])
-    tenure = st.slider("How many months has the customer been with the company?:",
+    partner = st.selectbox("Hat der Kunde einen Lebensgefährten?:",
+                          ["Ja", "Nein"])
+    if partner == "Ja":
+        partner = "Yes"
+    else:
+        partner = "No"
+    dependents = st.selectbox("Hat der Kunde Angehörige?:",
+                             ["Ja", "Nein"])
+    if dependents == "Ja":
+        dependents = "Yes"
+    else:
+        dependents = "No"
+    tenure = st.slider("Wie viele Monate ist der Kunde im Unternehmen?:",
                        min_value=0, max_value=72, value=42)
-    phone_service = st.selectbox("Does the customer have phone service?:",
-                                 ["Yes", "No"])
-    multiple_lines = st.selectbox("Does the customer have multiple lines?:",
-                                 ["Yes", "No", "No phone service"])
-    internet_service = st.selectbox("Does the customer have internet service?:",
-                                 ["No", "DSL", "Fiber optic"])
-    online_security = st.selectbox("Does the customer have online security?:",
-                                 ["Yes", "No", "No internet service"])
-    online_backup = st.selectbox("Does the customer have online backup?:",
-                                 ["Yes", "No", "No internet service"])
-    device_protection = st.selectbox("Does the customer have device protection?:",
-                                 ["Yes", "No", "No internet service"])
-    tech_support = st.selectbox("Does the customer have tech support?:",
-                                 ["Yes", "No", "No internet service"])
-    streaming_tv = st.selectbox("Does the customer have streaming TV??:",
-                                 ["Yes", "No", "No internet service"])
-    streaming_movies = st.selectbox("Does the customer have streaming movies?:",
-                                 ["Yes", "No", "No internet service"])
-    contract = st.selectbox("What kind of contract does the customer have?:",
-                                 ["Month-to-month", "Two year", "One year"])
-    paperless_billing = st.selectbox("Does the customer have paperless billing?:",
-                                     ["No", "Yes"])
-    payment_method = st.selectbox("What is the customer's payment method?:",
-                                  ["Mailed check", "Credit card (automatic)",
-                                   "Bank transfer (automatic)", "Electronic check"])
-    monthly_charges = st.slider("What is the customer's monthly charge?",
-                                min_value=0, max_value=118, value=50)
-    total_charges = st.slider("What is the total charge of the customer?",
-                              min_value=0, max_value=8600, value=2000)
+    phone_service = st.selectbox("Hat der Kunde einen Telefonservice?:",
+                                 ["Ja", "Nein"])
+    if phone_service == "Ja":
+        phone_service = "Yes"
+    else:
+        phone_service = "No"  
+    multiple_lines = st.selectbox("Hat der Kunde mehrere Leitungen?:",
+                                 ["Ja", "Nein", "Kein Telefonservice"])
+    if multiple_lines == "Ja":
+        multiple_lines = "Yes"
+    elif multiple_lines == "Nein":
+        multiple_lines = "No"
+    else:
+        multiple_lines = "No phone service"
+    internet_service = st.selectbox("Hat der Kunde Internetdienst?:",
+                                 ["Nein", "DSL", "Fiber Optic"])
+    if internet_service == "DSL":
+        internet_service = "DSL"
+    elif internet_service == "Fiber Optic":
+        internet_service = "Fiber optic"
+    else:
+        internet_service = "No"
+    online_security = st.selectbox("Hat der Kunde Online Security?:",
+                                 ["Ja", "Nein", "Kein Internet Service"])
+    if online_security == "Ja":
+        online_security = "Yes"
+    elif online_security == "Nein":
+        online_security = "No"
+    else:
+        online_security = "No internet service"
+
+    online_backup = st.selectbox("Hat der Kunde Online Backup?:",
+                                 ["Ja", "Nein", "Kein Internet Service"])
+    if online_backup == "Ja":
+        online_backup = "Yes"
+    elif online_backup == "Nein":
+        online_backup = "No"
+    else:   
+        online_backup = "No internet service"
+    device_protection = st.selectbox("Hat der Kunde Geräteschutz?:",
+                                    ["Ja", "Nein", "Kein Internet Service"])
+    if device_protection == "Ja":
+        device_protection = "Yes"
+    elif device_protection == "Nein":
+        device_protection = "No"
+    else:
+        device_protection = "No internet service"
+
+    tech_support = st.selectbox("Hat der Kunde Technischen Support?:",
+                                ["Ja", "Nein", "Kein Internet Service"])
+    if tech_support == "Ja":
+        tech_support = "Yes"
+    elif tech_support == "Nein":
+        tech_support = "No"
+    else:
+        tech_support = "No internet service"
+    streaming_tv = st.selectbox("Hat der Kunde Streaming TV?:",
+                                 ["Ja", "Nein", "Kein Internet Service"])
+    if streaming_tv == "Ja":
+        streaming_tv = "Yes"
+    elif streaming_tv == "Nein":
+        streaming_tv = "No"
+    else:
+        streaming_tv = "No internet service"
+    streaming_movies = st.selectbox("Hat der Kunde Streaming Movies?:",
+                                     ["Ja", "Nein", "Kein Internet Service"])
+    if streaming_movies == "Ja":
+        streaming_movies = "Yes"
+    elif streaming_movies == "Nein":
+        streaming_movies = "No"
+    else:
+        streaming_movies = "No internet service"
+    contract = st.selectbox("Welches Vertragsmodell hat der Kunde?:",
+                            ["Monatlich", "Zwei Jahre", "Ein Jahr"])
+    if contract == "Monatlich":
+        contract = "Month-to-month"
+    elif contract == "Zwei Jahre":
+        contract = "Two year"
+    else:
+        contract = "One year"
+    paperless_billing = st.selectbox("Hat der Kunde eine Papierlose Rechnung?:",
+                                        ["Ja", "Nein"])
+    if  paperless_billing == "Ja":
+        paperless_billing = "Yes"
+    else:
+        paperless_billing = "No"
+    payment_method = st.selectbox("Wie bezahlt der Kunde?:",
+                                ["Electronic Check", "Mailed Check", "Bank Transfer (Automatic)",
+                                "Credit Card (Automatic)"])
+    if payment_method == "Electronic Check":
+        payment_method = "Electronic check"
+    elif payment_method == "Mailed Check":
+        payment_method = "Mailed check"
+    elif payment_method == "Bank Transfer (Automatic)":
+        payment_method = "Bank transfer (automatic)"
+    else:
+        payment_method = "Credit card (automatic)"
+    monthly_charges = st.slider("Wie hoch sind die monatlichen Kosten?:",
+                                min_value=0.0, max_value=118.0, value=42.0)
+    total_charges = st.slider("Wie hoch sind die Gesamtkosten?:",
+                            min_value=0.0, max_value=8660.0, value=42.0)
+
+        # creating a dictionary of the inputs
+
 
     input_dict = {"gender": gender,
                   "SeniorCitizen": senior_citizen,
@@ -120,9 +207,9 @@ if __name__ == "__main__":
 
     input_data = pd.DataFrame([input_dict])
 
-    if st.button("Predict Churn"):
+    if st.button("Vorhersagen!"):
         pred = generate_predictions(input_data)
         if bool(pred):
-            st.error("Customer will churn!")
+            st.error("Kunde wird abwandern!")
         else:
-            st.success("Customer not predicted to churn")
+            st.success("Kunde wird wahrscheinlich bleiben!")
